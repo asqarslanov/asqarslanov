07e8578e8a94acb9608245eeaa2ab9b548823e62
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2023-10-30 16:48:17 +0300

    feat: links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<a href="https://asqars.dev">asqars.dev</a>
+<menu>
+  <a href="https://discordapp.com/users/397334384463577088">Discord</a>
+  <a href="https://t.me/AsqArslanov">Telegram</a>
+  <a href="https://github.com/asqarslanov">GitHub</a>
+</menu>
+<h1>Asqar Arslanov</h1>
+<h3>@AsqArslanov</h3>
