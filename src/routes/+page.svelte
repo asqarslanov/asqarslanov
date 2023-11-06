87c9bc961371207030c40842d81714ca3afce144
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2023-11-06 13:54:26 +0300

    refactor: discord link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <a href="https://asqars.dev">asqars.dev</a>
 <menu>
-  <a href="https://discordapp.com/users/397334384463577088">Discord</a>
+  <a href="https://discord.com/users/397334384463577088">Discord</a>
   <a href="https://t.me/AsqArslanov">Telegram</a>
   <a href="https://github.com/asqarslanov">GitHub</a>
 </menu>
