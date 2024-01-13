27033c9215bfdc1bd6dc136532438ca41203f50a
Asqar Arslanov <68478201+asqarslanov@users.noreply.github.com>
2024-01-13 13:47:16 +0500

    feat: leptos_i18n

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use crate::i18n::*;
 use leptos::*;
 
+leptos_i18n::load_locales!();
+
 fn main() {
+    provide_i18n_context();
     mount_to_body(App)
 }
 
 #[component]
-fn App() -> impl IntoView {
-    view! { <p>"AsqArs’ Website"</p> }
+pub fn App() -> impl IntoView {
+    let i18n = use_i18n();
+
+    view! { <p>{t!(i18n, init)}</p> }
 }
